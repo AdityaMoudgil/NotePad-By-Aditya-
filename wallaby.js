5235d046420db632fa875bec65175ac427695c96
Aditya Moudgil <72180284+IcodeAditya@users.noreply.github.com>
2021-06-29 11:36:49 +0530

    Add files via upload

---
+++ wallaby.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+    return {
+      files: [
+        'index.html',
+        'app.js',
+        'style.css'
+      ],
+      tests: [
+        ''
+      ],
+      env: {
+        kind: 'chrome'
+      },
+      debug: true
+    };
+  };
+  
